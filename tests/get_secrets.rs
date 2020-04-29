ed73631f23f406c1229a9ae731c6bd47e8adcd05
Nicholas Bishop <nicholasbishop@gmail.com>
2020-04-29 11:30:45 -0400

    tmp

--- tests/get_secrets.rs
+++ tests/get_secrets.rs
@@ -4,14 +4,21 @@ use std::{env, ffi::OsStr, os::unix::ffi::OsStrExt};
 
 type BoxError = Box<dyn std::error::Error>;
 
-#[derive(Deserialize)]
-struct License {
-    key: String,
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+struct Secret {
+    name: String,
 }
 
-#[derive(Deserialize)]
-struct LicenseResponse {
-    license: License,
+impl Secret {
+    fn new(name: &str) -> Secret {
+        Secret { name: name.into() }
+    }
+}
+
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+struct SecretsResponse {
+    total_count: u32,
+    secrets: Vec<Secret>,
 }
 
 fn get_var_bytes(name: &str) -> Result<Vec<u8>, BoxError> {
@@ -23,10 +30,11 @@ fn is_running_in_ci() -> bool {
     env::var_os("CI") == Some(OsStr::from_bytes(b"true").into())
 }
 
-// This test requires read-only access to the repository metadata.
+// This test requires read-only access to the repository secrets.
 #[test]
-fn get_metadata() -> Result<(), BoxError> {
+fn get_secrets() -> Result<(), BoxError> {
     if !is_running_in_ci() {
+        println!("skipping get_secrets test");
         return Ok(());
     }
 
@@ -44,19 +52,28 @@ fn get_metadata() -> Result<(), BoxError> {
         installation_id,
     })?;
 
-    let resp: LicenseResponse = token
+    let resp: SecretsResponse = token
         .client
-        .get(&format!("https://api.github.com/repos/{}/license", repo))
+        .get(&format!(
+            "https://api.github.com/repos/{}/actions/secrets",
+            repo
+        ))
         .headers(token.header()?)
         .send()?
         .error_for_status()?
         .json()?;
 
-    assert_eq!(resp, LicenseResponse {
-        license: License {
-            key: "apache-2.0",
-        },
-    });
+    assert_eq!(
+        resp,
+        SecretsResponse {
+            total_count: 3,
+            secrets: vec![
+                Secret::new("TEST_PRIVATE_KEY"),
+                Secret::new("TEST_APP_ID"),
+                Secret::new("TEST_INSTALLATION_ID")
+            ]
+        }
+    );
 
     println!("response: {:?}", resp);
 
